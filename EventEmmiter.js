3e793039f4a3d1d9228c946e7e841a5e2535d101
chparmar10@gmail.com <chparmar10@gmail.com>
2023-02-01 18:50:31 +0530

    Event Emmiter oprations

---
+++ EventEmmiter.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const EventEmmiter = require("events");
+
+const app = express();
+const event = new EventEmmiter();
+
+let count = 0;
+event.on("countAPI",()=>{
+    count++;
+    console.log("event called",count);
+});
+
+app.get("/",(req,res)=>{
+    res.send("api called");
+    event.emit("countAPI");
+});
+
+app.get("/search",(req,res)=>{
+    res.send("search api called");
+    event.emit("countAPI");
+});
+
+app.get("/update",(req,res)=>{
+    res.send("update api called");
+    event.emit("countAPI");
+});
+
+
+app.listen(4300);
